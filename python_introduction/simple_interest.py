829146bae8a1a1c942c9c47166657de57c29ee76
onyenike <onyenikefestus2019@gmail.com>
2025-05-22 14:13:11 -0700

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,6 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> P = 1000
->>> R = 0.05
->>> T = 3
->>> I = P * R * T
->>> print(I)
-150.0
+principal = 1000
+rate = 0.05
+time = 3
+Interest = principal * rate * time
+print(f"The simple is:{interest}")
+
