f1dc1336fcd26636f296f0e45034f6082d2d1a78
onyenike <onyenikefestus2019@gmail.com>
2025-05-22 16:24:04 -0700

    Create finance_calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+A = int(input("Enter your monthly income"))
+B = int(input("Enter your monthly expenses"))
+Monthly_Savings = A - B
+Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
+print(Projected_Savings)
+12600
