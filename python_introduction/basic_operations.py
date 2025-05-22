f3178cccf4f91255b9e69a7d1c5bfd4adfe6202d
onyenike <onyenikefestus2019@gmail.com>
2025-05-22 22:43:12 +0100

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> number1 = 10
+>>> number2 = 5
+>>> print(number1 + number2)
+15
+>>> print(number1 - number2)
+5
+>>> print(number1 * number2)
+50
