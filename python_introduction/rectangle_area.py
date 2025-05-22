49c0a2be7eaec2211b945e95b3a1070ce37e8c2c
onyenike <onyenikefestus2019@gmail.com>
2025-05-22 15:31:21 -0700

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,7 +1,5 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 >>> length = 10
 >>> width = 5
 >>> area = length * width
->>> print("The area of the rectangle is: " + str(area))
-The area of the rectangle is: 50
+>>> print(area)
+50
