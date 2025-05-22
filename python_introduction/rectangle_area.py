7c42cc5e9efb1b885ccfe13a1262e632c1fb47ba
onyenike <onyenikefestus2019@gmail.com>
2025-05-22 15:10:24 -0700

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,28 +1,3 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> lenght = 10
->>> width = 5
->>> area = lenght * width
->>> print("The area of the rectangle is:" area)
-SyntaxError: invalid syntax. Perhaps you forgot a comma?
->>> lenght = 10
-... width = 5
-... area = lenght * width
-... print("The area of the rectangle is:" + area)
-SyntaxError: multiple statements found while compiling a single statement
->>> lenght = 10
-... width = 5
-... area = lenght * width
-... print("The area of the rectangle is: " + str (area))
-SyntaxError: multiple statements found while compiling a single statement
->>> lenght = 10
-... width = 5
-... area = lenght * width
-... print("The area of the rectangle is: " + area)
-SyntaxError: multiple statements found while compiling a single statement
->>> lenght = 10
->>> width = 5
->>> area = lenght * area
 >>> lenght = 10
 >>> width = 5
 >>> area = lenght * width
