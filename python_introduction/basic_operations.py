007647087b3e5a7dd67620ee332145af773151d0
onyenike <onyenikefestus2019@gmail.com>
2025-05-22 15:34:03 -0700

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
->>> number1 = 10
->>> number2 = 5
->>> print(number1 + number2)
+number1 = 10
+number2 = 5
+print(number1 + number2)
 15
->>> print(number1 - number2)
+print(number1 - number2)
 5
->>> print(number1 * number2)
+print(number1 * number2)
 50
