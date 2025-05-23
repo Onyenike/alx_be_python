b25f92211d00ceb65a1010fcccf12181eb0311fc
onyenike <onyenikefestus2019@gmail.com>
2025-05-23 07:17:20 +0100

    Add files via upload

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,2 @@
-A = int(input("Enter your monthly income"))
-B = int(input("Enter your monthly expenses"))
-Monthly_Savings = A - B
-Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
-print(Projected_Savings)
-12600
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
