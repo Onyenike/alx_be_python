718fe960d6815f1fff0d90688485ee7e0e507e2d
onyenike <onyenikefestus2019@gmail.com>
2025-05-23 03:00:50 -0700

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-Monthlyincome =  int(input(“Enter your monthly income: ”))
-Monthlyexpenses = int(input(“Enter your total monthly expenses: ”))
-MonthlySavings = Monthly income -  Monthly expenses
-ProjectSavings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05))
-print(Project Savings) 
+Monthly_income =  int(input(“Enter your monthly income: ”))
+Monthly_expenses = int(input(“Enter your total monthly expenses: ”))
+Monthly_Savings = Monthly_income -  Monthly_expenses
+Project_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05))
+print(Project_Savings) 
 $12600
